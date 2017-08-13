a66c76400abf690d9cc9bb9fbd232a51e07d2b77
Edward Zhou <edward.zhq@gmail.com>
2017-08-13 23:31:41 +0800

    refactor implementation & test cases

--- PartI/step2/src/main/java/org/kata/RomanNumerals/RomanNumerals.java
+++ PartI/step2/src/main/java/org/kata/RomanNumerals/RomanNumerals.java
@@ -1,31 +1,55 @@
 package org.kata.RomanNumerals;
 
 public class RomanNumerals {
-  public String getRomanNumerals(int num) {
-    if (num == 1) {
-      return "I";
-    } else if (num == 2) {
-      return "II";
-    } else if (num == 3) {
-      return "III";
+
+  public String numberToString(int num, char unit, char half, char carry) {
+    char[] units = {unit, unit, unit};
+
+    if (num <= 0) {
+      return "";
+    } else if (num <4) {
+      return String.valueOf(units, 0, num);
     } else if (num == 4) {
-      return "IV";
+      return String.valueOf(new char[]{unit, half});
     } else if (num == 5) {
-      return "V";
-    } else if (num == 6) {
-      return "VI";
-    } else if (num == 7) {
-      return "VII";
-    } else if (num == 8) {
-      return "VIII";
+      return String.valueOf(half);
+    } else if (num < 9) {
+      return String.valueOf(half) + String.valueOf(units, 0, num - 5);
     } else if (num == 9) {
-      return "IX";
-    } else if (num == 10) {
-      return "X";
-    } else if (num == 11) {
-      return "XI";
+      return String.valueOf(new char[]{unit, carry});
+    } else {
+      return String.valueOf(carry);
+    }
+  }
+
+  public String getRomanNumerals(int num) {
+    StringBuffer buffer = new StringBuffer();
+
+    // N/A for out of range.
+    if (num >= 5000 || num <= 0) {
+      return "N/A";
     }
 
-    return "";
+    int digit;
+
+    // thousand
+    digit = num / 1000;
+    buffer.append(numberToString(digit, 'M', ' ', ' '));
+    
+    // centum
+    num = num % 1000;
+    digit = num / 100;
+    buffer.append(numberToString(digit, 'C', 'D', 'M'));
+    
+    // decade
+    num = num % 100;
+    digit = num / 10;
+    buffer.append(numberToString(digit, 'X', 'L', 'C'));
+    
+    // unit
+    digit = num % 10;
+    buffer.append(numberToString(digit, 'I', 'V', 'X'));
+    
+    return buffer.toString();
   }
 }
--- PartI/step2/src/test/java/org/kata/RomanNumberals/RomanNumeralsTest.java
+++ PartI/step2/src/test/java/org/kata/RomanNumberals/RomanNumeralsTest.java
@@ -23,8 +23,77 @@ public class RomanNumeralsTest {
   }
 
   @Test
-  public void testEleven() {
+  public void test_11() {
     assertEquals("XI", this.roman.getRomanNumerals(11));
   }
 
+  @Test
+  public void test_12() {
+    assertEquals("XII", this.roman.getRomanNumerals(12));
+  }
+
+  @Test
+  public void test_13() {
+    assertEquals("XIII", this.roman.getRomanNumerals(13));
+  }
+  
+  @Test
+  public void test_14() {
+    assertEquals("XIV", this.roman.getRomanNumerals(14));
+  }
+
+  @Test
+  public void test_20() {
+    assertEquals("XX", this.roman.getRomanNumerals(20));
+  }
+
+  @Test
+  public void test_40() {
+    assertEquals("XL", this.roman.getRomanNumerals(40));
+  }
+
+  @Test
+  public void test_49() {
+    assertEquals("XLIX", this.roman.getRomanNumerals(49));
+  }
+
+  @Test
+  public void test_50() {
+    assertEquals("L", this.roman.getRomanNumerals(50));
+  }
+
+  @Test
+  public void test_55() {
+    assertEquals("LV", this.roman.getRomanNumerals(55));
+  }
+
+  @Test
+  public void test_80() {
+    assertEquals("LXXX", this.roman.getRomanNumerals(80));
+  }
+
+  @Test
+  public void test_100() {
+    assertEquals("C", this.roman.getRomanNumerals(100));
+  }
+
+  @Test
+  public void test_369() {
+    assertEquals("CCCLXIX", this.roman.getRomanNumerals(369));
+  }
+
+  @Test
+  public void test_1998() {
+    assertEquals("MCMXCVIII", this.roman.getRomanNumerals(1998));
+  }
+
+  @Test
+  public void test_2751() {
+    assertEquals("MMDCCLI", this.roman.getRomanNumerals(2751));
+  }
+
+  @Test
+  public void test_5000() {
+    assertEquals("N/A", this.roman.getRomanNumerals(5000));
+  }
 }
